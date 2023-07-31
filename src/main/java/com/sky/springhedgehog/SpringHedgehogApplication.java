23b42f6a5081b7d0e10f23a8b6eae458ae7ce2b8
Matthew-Crowson-Sky <matthew.crowson@sky.uk>
2023-07-31 09:50:39 +0100

    Initial Commit

---
+++ src/main/java/com/sky/springhedgehog/SpringHedgehogApplication.java
@@ -0,0 +1,13 @@
+package com.sky.springhedgehog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHedgehogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHedgehogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/springhedgehog/SpringHedgehogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.springhedgehog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHedgehogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
